1d78af8eaa9cfe1c1468757fb5bfdae127808f89
Karsten Bolding <karsten@bolding-bruggeman.com>
2021-11-19 11:37:29 +0100

    updated run scripts to work on JRC cluster

--- medsea/medsea.py
+++ medsea/medsea.py
@@ -14,9 +14,9 @@ ERA_data_dirs = ('/data', '../../../igotm/data', '/ACQUA/COMMONDATA/ECMWF')
 BLUE2_setups_dir = next(filter(os.path.isdir, BLUE2_setups_dirs))
 TPXO9_data_dir = next(filter(os.path.isdir, TPXO9_data_dirs))
 ERA_data_dir = next(filter(os.path.isdir, ERA_data_dirs))
-ERA_path = os.path.join(ERA_data_dir, 'ERA-interim-org/analysis_2016.nc')
+#KB ERA_path = os.path.join(ERA_data_dir, 'ERA-interim-org/analysis_2016.nc')
 #JB ERA_path = os.path.join(ERA_data_dir, 'ERA-interim/2016.nc')
-#JRC ERA_path = os.path.join(ERA_data_dir, 'ncdf_erain/erain_2016_??.nc')
+ERA_path = os.path.join(ERA_data_dir, 'ncdf_erain/erain_2016_01.nc')
 
 domain = pygetm.legacy.domain_from_topo(os.path.join(BLUE2_setups_dir, setup, 'topo.nc'), nlev=30, z0_const=0.001)
 pygetm.legacy.load_bdyinfo(domain, os.path.join(BLUE2_setups_dir, setup, 'bdyinfo.dat'))
@@ -31,17 +31,21 @@ output = sim.output_manager.add_netcdf_file(os.path.join(outputdir, 'meteo.nc'),
 output.request(('u10', 'v10', 'sp', 't2m', 'd2m', 'tcc'))
 output = sim.output_manager.add_netcdf_file(os.path.join(outputdir, '2d.nc'), interval=240) # 240
 output.request(('U', 'V', 'zt'))
-output = sim.output_manager.add_netcdf_file(os.path.join(outputdir, '3d.nc', interval=1440)
+output = sim.output_manager.add_netcdf_file(os.path.join(outputdir, '3d.nc'), interval=1440)
 output.request(('dpdx', 'dpdy', 'tausxu', 'tausyv', 'u_taus'))
 output.request(('temp', 'salt', 'nuh', 'NN'))
 #output.request(('med_ergom_o2', 'med_ergom_OFL'))
 
 sim.logger.info('Setting up ERA meteorological forcing')
 era_kwargs = {'preprocess': lambda ds: ds.isel(time=slice(4, -4))}
+era_kwargs = {}
 sim.airsea.tcc.set(pygetm.input.from_nc(ERA_path, 'tcc', **era_kwargs))
-sim.airsea.t2m.set(pygetm.input.from_nc(ERA_path, 't2m', **era_kwargs) - 273.15)
-sim.airsea.d2m.set(pygetm.input.from_nc(ERA_path, 'd2m', **era_kwargs) - 273.15)
-sim.airsea.sp.set(pygetm.input.from_nc(ERA_path, 'sp', **era_kwargs))
+#KBsim.airsea.t2m.set(pygetm.input.from_nc(ERA_path, 't2m', **era_kwargs) - 273.15)
+sim.airsea.t2m.set(pygetm.input.from_nc(ERA_path, 't2', **era_kwargs) - 273.15)
+#KBsim.airsea.d2m.set(pygetm.input.from_nc(ERA_path, 'd2m', **era_kwargs) - 273.15)
+sim.airsea.d2m.set(pygetm.input.from_nc(ERA_path, 'dev2', **era_kwargs) - 273.15)
+#KBsim.airsea.sp.set(pygetm.input.from_nc(ERA_path, 'sp', **era_kwargs))
+sim.airsea.sp.set(pygetm.input.from_nc(ERA_path, 'slp', **era_kwargs))
 sim.airsea.u10.set(pygetm.input.from_nc(ERA_path, 'u10', **era_kwargs))
 sim.airsea.v10.set(pygetm.input.from_nc(ERA_path, 'v10', **era_kwargs))
 
--- swes/swes.py
+++ swes/swes.py
@@ -8,15 +8,15 @@ import pygetm.input.tpxo
 setup = 'swes'
 
 BLUE2_setups_dirs = ( '../../BLUE2', '../BLUE2', './BLUE2' )
-TPXO9_data_dirs = ( '/data', '../../../igotm/data', '/ACQUA' )
-ERA_data_dirs = ('/data', '../../../igotm/data', '/ACQUA/COMMONDATA/ECMWF/')
+TPXO9_data_dirs = ( '/data', '../../../igotm/data', '/ACQUA/COMMONDATA' )
+ERA_data_dirs = ('/data', '../../../igotm/data', '/ACQUA/COMMONDATA/ECMWF')
 
 BLUE2_setups_dir = next(filter(os.path.isdir, BLUE2_setups_dirs))
 TPXO9_data_dir = next(filter(os.path.isdir, TPXO9_data_dirs))
 ERA_data_dir = next(filter(os.path.isdir, ERA_data_dirs))
-ERA_path = os.path.join(ERA_data_dir, 'ERA-interim-org/analysis_2016.nc')
+#KB ERA_path = os.path.join(ERA_data_dir, 'ERA-interim-org/analysis_2016.nc')
 #JB ERA_path = os.path.join(ERA_data_dir, 'ERA-interim/2016.nc')
-#JRC ERA_path = os.path.join(ERA_data_dir, 'ncdf_erain/erain_2016_??.nc')
+ERA_path = os.path.join(ERA_data_dir, 'ncdf_erain/erain_2016_01.nc')
 
 domain = pygetm.legacy.domain_from_topo(os.path.join(BLUE2_setups_dir, setup, 'topo.nc'), nlev=30, z0_const=0.001)
 pygetm.legacy.load_bdyinfo(domain, os.path.join(BLUE2_setups_dir, setup, 'bdyinfo.dat'))
@@ -26,21 +26,26 @@ sim = pygetm.Simulation(domain, runtype=4, advection_scheme=1, gotm=os.path.join
 #sim.input_manager.debug_nc_reads()
 
 sim.logger.info('Setting up output')
-output = sim.output_manager.add_netcdf_file('meteo.nc', interval=1440) #1440
+outputdir = '/ACQUA/BLUE22/swes'
+output = sim.output_manager.add_netcdf_file(os.path.join(outputdir, 'meteo.nc'), interval=1440) #1440
 output.request(('u10', 'v10', 'sp', 't2m', 'd2m', 'tcc'))
-output = sim.output_manager.add_netcdf_file('2d.nc', interval=240) # 240
+output = sim.output_manager.add_netcdf_file(os.path.join(outputdir, '2d.nc'), interval=240) # 240
 output.request(('U', 'V', 'zt'))
-output = sim.output_manager.add_netcdf_file('3d.nc', interval=1440)
+output = sim.output_manager.add_netcdf_file(os.path.join(outputdir, '3d.nc'), interval=1440)
 output.request(('dpdx', 'dpdy', 'tausxu', 'tausyv', 'u_taus'))
 output.request(('temp', 'salt', 'nuh', 'NN'))
 #output.request(('med_ergom_o2', 'med_ergom_OFL'))
 
 sim.logger.info('Setting up ERA meteorological forcing')
 era_kwargs = {'preprocess': lambda ds: ds.isel(time=slice(4, -4))}
+era_kwargs = {}
 sim.airsea.tcc.set(pygetm.input.from_nc(ERA_path, 'tcc', **era_kwargs))
-sim.airsea.t2m.set(pygetm.input.from_nc(ERA_path, 't2m', **era_kwargs) - 273.15)
-sim.airsea.d2m.set(pygetm.input.from_nc(ERA_path, 'd2m', **era_kwargs) - 273.15)
-sim.airsea.sp.set(pygetm.input.from_nc(ERA_path, 'sp', **era_kwargs))
+#KBsim.airsea.t2m.set(pygetm.input.from_nc(ERA_path, 't2m', **era_kwargs) - 273.15)
+sim.airsea.t2m.set(pygetm.input.from_nc(ERA_path, 't2', **era_kwargs) - 273.15)
+#KBsim.airsea.d2m.set(pygetm.input.from_nc(ERA_path, 'd2m', **era_kwargs) - 273.15)
+sim.airsea.d2m.set(pygetm.input.from_nc(ERA_path, 'dev2', **era_kwargs) - 273.15)
+#KBsim.airsea.sp.set(pygetm.input.from_nc(ERA_path, 'sp', **era_kwargs))
+sim.airsea.sp.set(pygetm.input.from_nc(ERA_path, 'slp', **era_kwargs))
 sim.airsea.u10.set(pygetm.input.from_nc(ERA_path, 'u10', **era_kwargs))
 sim.airsea.v10.set(pygetm.input.from_nc(ERA_path, 'v10', **era_kwargs))
 
@@ -60,7 +65,7 @@ if domain.open_boundaries:
 
 sim.start(datetime.datetime(2016, 1, 1), timestep=15., split_factor=20)
 domain.update_depths() 
-while sim.time < datetime.datetime(2016, 1, 2):
+while sim.time < datetime.datetime(2016, 1, 5):
     sim.advance()
 
 sim.finish()
